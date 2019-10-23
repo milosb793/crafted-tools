5001652d3e41b873d96843317a2b780ae21f6b89
Milosh <milosb793@gmail.com>
2019-10-23 23:58:50 +0200

    Update

---
+++ linux/bash/installations/debian/install_common_server_packages.sh
@@ -0,0 +1,16 @@
+#/bin/env bash
+
+# Installing other packages
+echo "--------------------"
+echo "Installing other packages..."
+echo "--------------------"
+sudo apt-get install git -y;
+sudo apt-get install curl axel -y;
+sudo apt-get install screen -y;
+sudo apt-get install sshpass -y;
+sudo apt-get install zip -y;
+sudo apt-get install composer -y;
+sudo apt install redis-server -y;
+sudo apt install supervisor -y;
+sudo apt install htop -y;
+sudo apt install jq -y;
---
+++ linux/bash/installations/debian/install_nginx.sh
@@ -0,0 +1,7 @@
+#/bin/env bash
+
+# Installing NGINX
+echo "--------------------"
+echo "Installing NGINX"
+echo "--------------------"
+sudo apt-get install nginx;
